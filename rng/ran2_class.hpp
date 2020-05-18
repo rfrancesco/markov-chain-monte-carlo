81145384703b6c02634eec6d9d2d0539907b0950
Rocco Francesco Basta <r.basta@studenti.unipi.it>
2020-05-18 21:28:05 +0200

    rng: Added ran2_class.hpp (does not contain algorithm)

---
+++ rng/ran2_class.hpp
@@ -0,0 +1,35 @@
+#ifndef RAN2_H
+#define RAN2_H
+
+#include <cfloat>
+#include <cmath>
+#include <iostream>
+
+// Ran2 class header
+// This is a dummy header without the accompanying .cpp file.
+
+#define NTAB 32
+
+class Ran2 {
+    private:
+	// Internal state
+        std::int64_t idum;
+        std::int64_t idum2;
+        std::int64_t iy;
+        std::int64_t iv[NTAB];
+	// Box-Muller buffer (generates 2 numbers, keeps 1 stored for easy retrieval)
+        double boxmuller_buffer;
+        bool boxmuller_buffer_full = false;
+    public:
+	// Constructor - takes integer seed > 0 (sign is flipped inside the constructor)
+        explicit Ran2(std::int64_t);
+	// Generate random double (uniform deviates)
+        double doub();
+    	// Generate gaussian double with Box-Muller algorithm
+        double gaussian(double, double);
+};
+
+#undef NTAB
+
+
+#endif
