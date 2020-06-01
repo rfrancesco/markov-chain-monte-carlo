19954a346b630bd467a50ce224dd19e0b18ce277
Rocco Francesco Basta <r.basta@studenti.unipi.it>
2020-06-01 18:54:01 +0200

    s1: commented for exam

--- qm/circle/s1.hpp
+++ qm/circle/s1.hpp
@@ -7,17 +7,22 @@ class S1 {
         double x;
         void wrap();
     public:
+	// Constructor / Destructor
         S1() :x{0} {}
         S1(const S1&);
         explicit S1(double a) { x = a; wrap(); }
+	// Cast to double
         operator double() const { return x; }
+	// Assignment and group operations (+, -)
         S1& operator =(double);
         S1& operator =(S1);
         S1& operator +=(double);
         S1& operator -=(double);
+	// Print to ostream
         std::ostream& operator<< (std::ostream&);
 };
 
+// Group operations (+, -) with S1 and with doubles
 S1 operator +(double, S1);
 S1 operator +(S1, double);
 S1 operator +(S1, S1);
